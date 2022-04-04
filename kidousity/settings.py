69f1827b5848d0028b619e0e4cd9abd7453d0ffa
Pulkit Khandelwal <pulkit.khandelwal29@gmail.com>
2022-04-04 10:38:16 +0530

    Update settings.py

--- kidousity/settings.py
+++ kidousity/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -116,6 +118,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#This will create the static folder and will access everything from this directory
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+#where to look for all our staticfiles(here we have keep data --  photo)
+STATICFILES_DIRS= [
+    os.path.join(BASE_DIR,'kidousity/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
